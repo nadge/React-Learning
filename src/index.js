3c2cf487420310ff8fb8d3bc284fd590eee73ac2
Neil Aldred <neil.aldred@soapmedia.co.uk>
2016-04-13 13:45:32 +0100

    git ignore added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Create new component
+// This component should produce some HTML
+const App = function () {
+    return <div>Hi!</div>;
+}
+
+
+// Take this components generated HTML and put it on the page (in the DOM)
+React.render(App);
